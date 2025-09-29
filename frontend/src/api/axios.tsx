a2990d87afd0e4847de2992b39a2a43f28390157
Tharindu lakmal <161035317+tharindu748@users.noreply.github.com>
2025-09-29 22:27:41 +0530

    update

--- frontend/src/api/axios.tsx
+++ frontend/src/api/axios.tsx
@@ -2,7 +2,7 @@ import axios, { AxiosError } from "axios";
 import { useAuth } from "@/context/auth-context";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.VITE_API_BASE_URL||"http://localhost:3000",
   withCredentials: true,
 });
 
